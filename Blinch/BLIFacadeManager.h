385ab3ad952dafae364810abc67a4c0ec04b9ddb
Markus Kopf <kopf.markus@googlemail.com>
2015-01-06 14:26:44 +0100

    Initial high level architecture setup + facade manager as singelton

---
+++ Blinch/BLIFacadeManager.h
@@ -0,0 +1,13 @@
+//
+//  BLIFacadeManager.h
+//  Blinch
+//
+//  Created by Markus Kopf on 06/01/15.
+//  Copyright (c) 2015 Oberst Tanja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BLIFacadeManager : NSObject
+
+@end
---
+++ Blinch/BLIFacadeManager.m
@@ -0,0 +1,55 @@
+//
+//  BLIFacadeManager.m
+//  Blinch
+//
+//  Created by Markus Kopf on 06/01/15.
+//  Copyright (c) 2015 Oberst Tanja. All rights reserved.
+//
+
+#import "BLIFacadeManager.h"
+
+@implementation BLIFacadeManager
+
+static BLIFacadeManager *sharedInstance = nil;
+
++ (BLIFacadeManager *)sharedInstance
+{
+    @synchronized(self)
+    {
+        if (sharedInstance == nil)
+        {
+            sharedInstance = [[self alloc] init];
+        }
+    }
+    
+    return sharedInstance;
+}
+
++ (id)allocWithZone:(NSZone *)zone
+{
+    @synchronized(self)
+    {
+        if (sharedInstance == nil)
+        {
+            sharedInstance = [super allocWithZone:zone];
+            return sharedInstance;
+        }
+    }
+    
+    return nil;
+}
+
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+ 
+    return self;
+}
+
+
+
+@end
